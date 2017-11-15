6d1f280c8bc4a057302f16911b180302a3258f4d
Sebastian Sauer <ssauer@posteo.de>
2017-11-15 12:30:35 +0100

    figure support

--- vignettes/yart.R
+++ vignettes/yart.R
@@ -8,3 +8,6 @@ options(xtable.comment = FALSE)
 xtable(head(mtcars[,1:3]), caption = "Eine krasse Tabelle", label = "tab:Tab2")
 
 
+## ----fig1, echo = TRUE, fig.cap = "Example of a figure", out.width = "50%"----
+knitr::include_graphics("../docs/picture2.png")
+
