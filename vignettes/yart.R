6c5ba21c3c89f89d27cefceae04735d555deb071
Sebastian Sauer <ssauer@posteo.de>
2017-11-15 22:49:44 +0100

    compress pdf

--- vignettes/yart.R
+++ vignettes/yart.R
@@ -2,12 +2,6 @@
 x <- c(1,2,3)
 mean(x)
 
-## ----Tab2, echo = FALSE, results = "asis", message = FALSE---------------
-library(xtable)
-options(xtable.comment = FALSE)
-xtable(head(mtcars[,1:3]), caption = "Eine krasse Tabelle", label = "tab:Tab2")
-
-
 ## ----fig1, echo = TRUE, fig.cap = "Example of a figure", out.width = "50%"----
 #knitr::include_graphics("../docs/picture2.png")
 plot(mtcars$hp)
